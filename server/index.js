6ecabd078a2da83808d4831a85a06d6ceded56e7
Taiki-nk <taikifate.0206@gmail.com>
2022-12-09 23:08:10 +0900

    :+1:[feat] mongodb接続

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = 3000;
+require("dotenv").config();
 
-app.get("/", (req, res) => {
-  res.send("Hello Express");
-});
+mongoose.set("strictQuery", false);
+
+// DB接続
+try {
+  mongoose.connect(process.env.MONGODB_URL);
+  console.log("DB接続中");
+} catch (error) {
+  console.log(error);
+}
 
 app.listen(PORT, () => {
   console.log("ローカルサーバー起動中...");
