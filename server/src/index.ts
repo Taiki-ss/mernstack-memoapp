5bbc7281316a31a22b4f1f62f5922e52484675f7
Taiki-nk <taikifate.0206@gmail.com>
2022-12-15 22:09:09 +0900

    :up:[fix] ts init

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,10 @@
-require('./src/v1/db/connect');
+require('./v1/db/connect');
 const express = require('express');
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-app.use('/api/v1', require('./src/v1/routes/auth'));
+app.use('/api/v1', require('./v1/routes/auth'));
 
 app.listen(PORT, () => {
   console.log('ローカルサーバー起動中...');
