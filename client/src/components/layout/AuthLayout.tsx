503f0b661707ceb042cf1942ddb1c7da2940f397
Taiki-nk <taikifate.0206@gmail.com>
2022-12-18 14:38:59 +0900

    :+1:[feat] スタイル調整

--- client/src/components/layout/AuthLayout.tsx
+++ client/src/components/layout/AuthLayout.tsx
@@ -1,12 +1,31 @@
+// カスタム型を定義
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { Container, Box } from "@mui/material";
+import notionLogo from "../../assets/images/notion-logo.png";
 
 const AuthLayout = () => {
   return (
-    <div>
-      AuthLayout
-      <Outlet />
-    </div>
+    <>
+      <Container maxWidth="xs">
+        <Box
+          sx={{
+            marginTop: 6,
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          <img
+            src={notionLogo}
+            alt=""
+            style={{ width: 100, height: 100, marginTop: 3 }}
+          />
+          Notion
+        </Box>
+        <Outlet />
+      </Container>
+    </>
   );
 };
 
--- client/src/react-app-env.d.ts
+++ client/src/react-app-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="react-scripts" />
+
+// カスタム型を定義
+declare module "*.png" {
+  const value: string;
+  export default value;
+}
