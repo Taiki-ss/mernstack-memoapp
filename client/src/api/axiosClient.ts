96a96b4b466ef5a47e621ed0a03b0dc07efa5a94
Taiki-nk <taikifate.0206@gmail.com>
2022-12-18 21:29:18 +0900

    :sparkles:[feat] axiosClient作成

---
+++ client/src/api/axiosClient.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:3100/api/v1";
+const getToken = () => localStorage.getItem("token");
+
+const axiosClient = axios.create({
+  baseURL: BASE_URL,
+});
+
+// APIを叩く前に前処理を行う
+axiosClient.interceptors.request.use(
+  async (config) => {
+    return {
+      config,
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${getToken()}`, //リクエストヘッダにJWTをつけてサーバーに渡す
+      },
+    };
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
