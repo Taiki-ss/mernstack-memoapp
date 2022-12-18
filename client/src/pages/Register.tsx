5d75e46b6f473d609f729f4318e28b57986bcd1a
Taiki-nk <taikifate.0206@gmail.com>
2022-12-18 15:43:10 +0900

    :sparkles:[feat] 新規登録フォーム作成

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,7 +1,57 @@
 import React from "react";
+import { Box, TextField, Button } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <>
+      <Box component="form">
+        <TextField
+          fullWidth
+          margin="normal"
+          id="username"
+          name="username"
+          label="お名前"
+          required
+        />
+        <TextField
+          fullWidth
+          margin="normal"
+          type="password"
+          id="password"
+          name="password"
+          label="パスワード"
+          required
+        />
+        <TextField
+          fullWidth
+          margin="normal"
+          type="password"
+          id="confirmPassword"
+          name="confirmPassword"
+          label="確認用パスワード"
+          required
+        />
+        <LoadingButton
+          sx={{
+            mt: 3,
+            mb: 2,
+          }}
+          fullWidth
+          type="submit"
+          loading={false}
+          color="primary"
+          variant="outlined"
+        >
+          アカウント作成
+        </LoadingButton>
+      </Box>
+      <Button component={Link} to="/login">
+        すでにアカウントを持っていますか？ログイン
+      </Button>
+    </>
+  );
 };
 
 export default Register;
