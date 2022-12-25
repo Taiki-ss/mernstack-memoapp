9b10e91823e9c252c7e3fb0b7476a2b472cc0d40
Taiki-nk <taikifate.0206@gmail.com>
2022-12-25 12:55:59 +0900

    :+1:[feat] サイドバー雛形完成

--- client/src/components/common/Sidebar.tsx
+++ client/src/components/common/Sidebar.tsx
@@ -1,7 +1,77 @@
 import React from "react";
+import {
+  Drawer,
+  IconButton,
+  List,
+  ListItemButton,
+  Typography,
+} from "@mui/material";
+import { Box } from "@mui/system";
+import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
+import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <Drawer
+      container={window.document.body}
+      variant="permanent"
+      open={true}
+      sx={{ width: 250, height: "100vh" }}
+    >
+      <List sx={{ width: 250, height: "100vh" }}>
+        <ListItemButton>
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography variant="body2" fontWeight="700">
+              taiki
+            </Typography>
+            <IconButton>
+              <LogoutOutlinedIcon />
+            </IconButton>
+          </Box>
+        </ListItemButton>
+        <ListItemButton>
+          <Box
+            sx={{
+              paddingTop: "10px",
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography variant="body2" fontWeight="700">
+              お気に入り
+            </Typography>
+          </Box>
+        </ListItemButton>
+        <ListItemButton>
+          <Box
+            sx={{
+              paddingTop: "10px",
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography variant="body2" fontWeight="700">
+              プライベート
+            </Typography>
+            <IconButton>
+              <AddBoxOutlinedIcon />
+            </IconButton>
+          </Box>
+        </ListItemButton>
+      </List>
+    </Drawer>
+  );
 };
 
 export default Sidebar;
